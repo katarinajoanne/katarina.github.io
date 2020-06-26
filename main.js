4af024e6dbbff9076b725a5c8bd857dc6882334d
Katarina Keishanti Joanne <katarina.keisha@gmail.com>
2020-06-26 10:07:22 +0700

    revising content

--- main.js
+++ main.js
@@ -3,4 +3,27 @@ $(document).ready(function () {
       $(this).toggleClass('open');
       $('.top-nav').toggleClass('open');
     });
-});
+
+    $ ('.top-nav .nav-link').on('click', function() {
+      $('.menu-toggler').removeClass('open');
+      $('.top-nav').removeClass('open');
+    });
+
+    //$('nav a[*="#"]').on('click', function() {
+      //$('html, body').animate (keyframe, {
+        //scrollTop: $($(this).attr('href')).offset().top - 100
+      //}, options, 2000);
+    //});
+
+    //$ ('#up').on('click', function() {
+      //$('html, body').animate( keyframes, {
+        //scrollTop: 0
+      // }, options, 2000);
+    }); 
+
+   //  A0S.init({
+     // easing: 'ease',
+     // duration: 1800,
+     // once: true
+   // }) 
+// }); 
